fdb1685e1b658220f510700438c3632db5bd0d62
Aysegul Yonet <aysegulyonet@gmail.com>
2015-08-20 01:18:13 -0700

    Ring configuration objects created.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,8 +7,6 @@ var redLine = d3.svg.line()
 	.y(function(d) { return d.y; })
 	.interpolate("linear");
 
-
-
 //Description for the orange line definition function.
 //100px offset wasn't matching the image so I did change it to 180 instead.
 var orangeLine = d3.svg.line()
@@ -28,23 +26,117 @@ var svg = d3.select("#container").append("svg")
 
 //appends red line
 var redLine = svg.append("path")
-	.attr("d", redLine(lineData))
-	.attr("stroke", "red")
-	.attr("stroke-width", 5)                           
-	.attr("fill", "none");
+	.attr({
+		"d": redLine(lineData),
+		"stroke": "red",
+		"stroke-width": 5,
+		"fill": "none"
+	})
 
 // appends orange line
 var orangeLine = svg.append("path")
-	.attr("d", orangeLine(lineData))
-	.attr("stroke", "orange")
-	.attr("stroke-width", 2)                           
-	.attr("fill", "none");
+	.attr({
+		"d": orangeLine(lineData),
+		"stroke": "orange",
+		"stroke-width": 2,
+		"fill", "none"
+	});
 
 var orangeLine2 = svg.append("path")
-	.attr("d", orangeLine2(lineData))
 	.attr({
+		"d": orangeLine2(lineData),
 		"stroke": "orange",
 		"stroke-width": 20,
 		"opacity": 0.2
 	})
 
+//Callout 1
+var callouts = {
+	one: [
+		{
+			"d": blueRing,
+			"stroke": "blue",
+			"stroke-width": ,
+			"opacity": 1
+		},
+		{
+			"d": whiteRing,
+			"stroke": "white",
+			"stroke-width": 2,
+			"opacity": 1
+		},
+		{
+			"d": blueTransparentRing,
+			"stroke": "white",
+			"stroke-width": 10,
+			"opacity": 0,5
+		}
+
+	],
+	two: [
+		{
+			"d": lightBlueRing,
+			"stroke": "light-blue",
+			"stroke-width": 5,
+			"opacity": 1
+		},
+		{
+			"d": blueTransparentRing,
+			"stroke": "white",
+			"stroke-width": 10,
+			"opacity": 0,5
+		}
+
+	]
+}
+
+
+
+var createCallout = function(type){
+	var circle = svg.append("circle")
+      .attr("transform", "translate('500', '500')")
+      .style("cursor", "pointer")
+  if(type === "calloutOne"){
+  	circle.attr({
+  		"r": 5,
+  		"stroke", "while",
+  		"stroke-width": 2
+  	})
+  }
+  circle.call(drag);
+	return circle;
+}
+
+function click(e){
+  // Ignore the click event if it was suppressed
+  if (d3.event.defaultPrevented) return;
+  var circle = e.target.id;
+  console.log('circle clicked', circle);
+  // Extract the click location\    
+  var point = d3.mouse(this)
+  , p = {x: point[0], y: point[1] };
+
+  // Append a new callout
+  var callout = createCallout(circle);
+  
+  // var circle = svg.append("circle")
+  //     .attr("transform", "translate('500', '500')")
+  //     .attr("class", circleClass)
+  //     .style("cursor", "pointer")
+  //     .call(drag);
+}
+
+
+// Define drag beavior
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+function dragmove(d) {
+  var x = d3.event.x;
+  var y = d3.event.y;
+
+  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+}
+
+var header = d3.select('.header');
+header.addEventListener('click', click);
